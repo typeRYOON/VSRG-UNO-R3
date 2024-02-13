85d8be33d4871791f6b3088fafdd1143872c3421
Ryan <ryamagdaleno@gmail.com>
2024-02-13 17:41:08 -0600

    : : Source Files

--- computer/src/Keystrokes-Display/main.cpp
+++ computer/src/Keystrokes-Display/main.cpp
@@ -1,7 +1,21 @@
-#include <iostream>
+#include "..\..\include\Keystrokes-Display\osuenv.h"
+#include "..\..\include\Keystrokes-Display\keychecker.h"
+std::string DR = R"(C:\Users\Esperanza\AppData\Local\osu!)";
+
 
 int main()
 {
-    
+    // Create OsuEnv object ::
+    OsuEnv* env = nullptr;
+    try { env = new OsuEnv(DR); }
+    catch (const std::invalid_argument& e)
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return -1;
+    }
+    KeyChecker* keyChecker = new KeyChecker(*env);
+
+    keyChecker->trackKeys(7);
+
     return 0;
 }
